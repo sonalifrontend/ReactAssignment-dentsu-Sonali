491c3d9a065af5fcc6824c123fc7744e1b2e0d4c
sonali jaiswal <sonalijaiswal0108@gmail.com>
2021-03-13 00:47:52 +0530

    commit for dynamic card grid
    committed the code for dynamic responsive card grid

--- card-app/src/App.js
+++ card-app/src/App.js
@@ -1,78 +1,21 @@
 import './App.css';
-import Card from './components/Card'
-import './components/Card.css'
+import ShowCards from './components/Card'
 
-function App() {
-  return (
-    <div class="container-fluid">
-      <h1>Card Assignment</h1>
-      <div className="row is-table-row">
-      <div className="col-lg-3 col-xs-12 col-md-6 card-block">
-        <a href="https://google.com" target="_blank" rel="noopener noreferrer" title="Open in new window">
-          <Card 
-            title="Card Title"
-            imageUrl="https://images.unsplash.com/photo-1612831197310-ff5cf7a211b6?ixid=MnwxMjA3fDB8MXx0b3BpY3x8YWV1NnJMLWo2ZXd8fHx8fDJ8fDE2MTUzNjgxMTc&ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=120&h=200&q=60"
-            body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimenooooooooooooooooooooooooooo book. It has survived not "
-          />
-        </a>
-      </div>
-      <div className="col-lg-3 col-xs-12 col-md-6 card-block">
-        <a href="https://google.com" target="_blank" rel="noopener noreferrer" title="Open in new window">
-          <Card 
-            title="Card Title"
-            imageUrl="https://images.unsplash.com/photo-1612831197310-ff5cf7a211b6?ixid=MnwxMjA3fDB8MXx0b3BpY3x8YWV1NnJMLWo2ZXd8fHx8fDJ8fDE2MTUzNjgxMTc&ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=120&h=200&q=60"
-            body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not."
-          />
-        </a>
-      </div>
-      <div className="col-lg-3 col-xs-12 col-md-6 card-block">
-        <a href="https://google.com" target="_blank" rel="noopener noreferrer" title="Open in new window">
-          <Card 
-            title="Card Title"
-            imageUrl="https://images.unsplash.com/photo-1612831197310-ff5cf7a211b6?ixid=MnwxMjA3fDB8MXx0b3BpY3x8YWV1NnJMLWo2ZXd8fHx8fDJ8fDE2MTUzNjgxMTc&ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=120&h=200&q=60"
-            body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not."
-          />
-        </a>
-      </div>
-      <div className="col-lg-3 col-xs-12 col-md-6 card-block">
-        <a href="https://google.com" target="_blank" rel="noopener noreferrer" title="Open in new window">
-          <Card 
-            title="Card Title"
-            imageUrl="https://images.unsplash.com/photo-1612831197310-ff5cf7a211b6?ixid=MnwxMjA3fDB8MXx0b3BpY3x8YWV1NnJMLWo2ZXd8fHx8fDJ8fDE2MTUzNjgxMTc&ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=120&h=200&q=60"
-            body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not."
-          />
-        </a>
-      </div>
-      <div className="col-lg-3 col-xs-12 col-md-6 card-block">
-        <a href="https://google.com" target="_blank" rel="noopener noreferrer" title="Open in new window">
-          <Card 
-            title="Card Title"
-            imageUrl="https://images.unsplash.com/photo-1612831197310-ff5cf7a211b6?ixid=MnwxMjA3fDB8MXx0b3BpY3x8YWV1NnJMLWo2ZXd8fHx8fDJ8fDE2MTUzNjgxMTc&ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=120&h=200&q=60"
-            body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not.jsssssssssss"
-          />
-        </a>
-        </div>
-        <div className="col-lg-3 col-xs-12 col-md-6 card-block">
-          <a href="https://google.com" target="_blank" rel="noopener noreferrer" title="Open in new window">
-            <Card 
-              title="Card Title"
-              imageUrl="https://images.unsplash.com/photo-1612831197310-ff5cf7a211b6?ixid=MnwxMjA3fDB8MXx0b3BpY3x8YWV1NnJMLWo2ZXd8fHx8fDJ8fDE2MTUzNjgxMTc&ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=120&h=200&q=60"
-              body="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not."
-            />
-        </a>
-        </div>
+const CardDetails = () => {
+  const cardInfo = [
+  {image:"https://images.unsplash.com/photo-1612831197310-ff5cf7a211b6?ixid=MnwxMjA3fDB8MXx0b3BpY3x8YWV1NnJMLWo2ZXd8fHx8fDJ8fDE2MTUzNjgxMTc&ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=120&h=200&q=60",title:"This is my Title", info:"In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder beforeIn publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available. final copy is available."},
+  {image:"https://images.unsplash.com/photo-1612831197310-ff5cf7a211b6?ixid=MnwxMjA3fDB8MXx0b3BpY3x8YWV1NnJMLWo2ZXd8fHx8fDJ8fDE2MTUzNjgxMTc&ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=120&h=200&q=60",title:"This is my Title", info:"In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available."},
+  {image:"https://images.unsplash.com/photo-1612831197310-ff5cf7a211b6?ixid=MnwxMjA3fDB8MXx0b3BpY3x8YWV1NnJMLWo2ZXd8fHx8fDJ8fDE2MTUzNjgxMTc&ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=120&h=200&q=60",title:"This is my Title", info:"In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder beforIn publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available.e final copy is available."},
+  {image:"https://images.unsplash.com/photo-1612831197310-ff5cf7a211b6?ixid=MnwxMjA3fDB8MXx0b3BpY3x8YWV1NnJMLWo2ZXd8fHx8fDJ8fDE2MTUzNjgxMTc&ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=120&h=200&q=60",title:"This is my Title", info:"In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available."},
+  {image:"https://images.unsplash.com/photo-1612831197310-ff5cf7a211b6?ixid=MnwxMjA3fDB8MXx0b3BpY3x8YWV1NnJMLWo2ZXd8fHx8fDJ8fDE2MTUzNjgxMTc&ixlib=rb-1.2.1&dpr=1&auto=format&fit=crop&w=120&h=200&q=60",title:"This is my Title", info:"In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available."},
+  {image:"",title:"This is my Title", info:"In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before final copy is available."},
+  ]
+
+return (
+    <div className="App row">  
+      {cardInfo.map(ShowCards)}  
     </div>
-      
-        
-     
-
-      
-      
-      </div>
+)};
 
 
-
-  );
-}
-
-export default App;
+export default CardDetails;
--- card-app/src/components/Card.js
+++ card-app/src/components/Card.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import './Card.css'
+import { Card } from "react-bootstrap";
 
-function Card({title, imageUrl, body}) {
-	return (
-			
-		<>
-			<div className="image-container util-margin-bottom-10"><img src={imageUrl} alt="img" className="" /></div>
-			<div className="body-container">
-			<h2 className="h3">{title}</h2>
-				<p>{body}</p>
-			</div>
-									 
-		</>
-	)
+const ShowCards = (card, index) => {
+
+  	const showImage = card.image;
+
+  	return(
+    
+	    <div className="col-md-6 col-lg-4 col-xs-12 util-margin-bottom-10">
+		    <a href="https://google.com" target="_blank" rel="noopener noreferrer" title="Open in new window">
+			    <Card style={{height:"100%"}} key={index} >
+					{showImage ? <Card.Img variant="top" src={card.image} alt="image" height="40%" /> : ""}
+					<Card.Body>
+					    <Card.Title>{card.title}</Card.Title>
+					    <Card.Text>
+					      {card.info}
+					    </Card.Text>
+					</Card.Body>
+			    </Card>
+		    </a>
+	    </div>
+    
+    )
 }
 
-export default Card
+export default ShowCards
